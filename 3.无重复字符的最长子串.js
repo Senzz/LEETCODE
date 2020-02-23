0422550cbde60974b6c74b147a3db1ac86922ef0
zoujianquan <zoujianquan@huobideMacBook-Pro.local>
2020-02-23 16:53:44 +0800

    [滑动窗口]

---
+++ 3.无重复字符的最长子串.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=javascript
+ *
+ * [3] 无重复字符的最长子串
+ * 提示：滑动窗口
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var lengthOfLongestSubstring = function(s) {
+  let max = 0
+  let left = 0
+  const hash = {}
+  for (let right = 0; right < s.length; right++) {
+    const moveLeft = hash[s[right]]
+    if (moveLeft) {
+      // 有相同的两个字符, 相同字符之间的长度
+      // 找到上一个字符的位置，即窗口left位置
+      // 但不能比现在的left更小（即更左边）, 因为这段已有重复的隔断了, 例子：abba, 即有bb了
+      // 所以需要Math.max
+      left = Math.max(left, moveLeft)
+    }
+    hash[s[right]] = right + 1
+    max = Math.max(max, right - left + 1)
+  }
+  return max
+};
+// @lc code=end
+
