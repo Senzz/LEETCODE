93e651796c51ccf188fc2a872784965688dcc334
Sen <senjqz@gmail.com>
2022-05-08 19:03:43 +0800

    feat: 160.相交链表 双指针

--- 160.相交链表.js
+++ 160.相交链表.js
@@ -18,20 +18,35 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
+// 哈希集合
+// var getIntersectionNode = function (headA, headB) {
+//   const set = new Set()
+//   let temp = headA
+//   while (temp !== null) {
+//     set.add(temp)
+//     temp = temp.next
+//   }
+
+//   temp = headB
+
+//   while (temp) {
+//     if (set.has(temp)) {
+//       return temp
+//     }
+//     temp = temp.next
+//   }
+//   return null
+// };
+
+// 双指针
 var getIntersectionNode = function (headA, headB) {
-  const visited = new Set()
-  let temp = headA
-  while (temp !== null) {
-    visited.add(temp)
-    temp = temp.next
+  let a = headA
+  let b = headB
+  while (a !== b) {
+    a = a === null ? headB : a.next
+    b = b === null ? headA : b.next
   }
-  temp = headB
-  while(temp !== null) {
-    if (visited.has(temp)) {
-      return temp
-    }
-    temp = temp.next
-  }
-  return null
+
+  return a || null
 };
 // @lc code=end
